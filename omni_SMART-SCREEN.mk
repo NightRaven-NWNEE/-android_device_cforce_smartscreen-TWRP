37c3d0a29052cd13726d8dd9ea703d5ad3be5700
NightRaven <nrtf526@outlook.com>
2022-06-14 16:40:01 -0500

    SMART-SCREEN: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: c-force Model: CF011S Script version: 1.3.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_SMART-SCREEN.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from SMART-SCREEN device
+$(call inherit-product, device/c-force/SMART-SCREEN/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := SMART-SCREEN
+PRODUCT_NAME := omni_SMART-SCREEN
+PRODUCT_BRAND := C-FORCE
+PRODUCT_MODEL := CF011S
+PRODUCT_MANUFACTURER := c-force
+PRODUCT_RELEASE_NAME := C-FORCE CF011S
